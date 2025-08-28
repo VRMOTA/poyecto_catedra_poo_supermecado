f3b9552b7146e04446083289792d9e13f1de54d4
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-28 01:10:16 -0600

    paneles

---
+++ poyecto_catedra_poo_supermecado/frm_catalogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace poyecto_catedra_poo_supermecado
+{
+    public partial class frm_catalogo : Form
+    {
+        public frm_catalogo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ poyecto_catedra_poo_supermecado/frm_consultas.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace poyecto_catedra_poo_supermecado
+{
+    partial class frm_consultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_consultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1279, 911);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_consultas";
+            this.Text = "frm_consultas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
