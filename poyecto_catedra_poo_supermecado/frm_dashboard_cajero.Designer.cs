28a685456f1b50cd32b1c796c100b06985d93687
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-28 00:25:07 -0600

    Dahsboard

---
+++ poyecto_catedra_poo_supermecado/frm_dashboard_cajero.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace poyecto_catedra_poo_supermecado
+{
+    partial class frm_dashboard_cajero
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_dashboard_cajero));
+            this.navegador = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnl_menu = new System.Windows.Forms.Panel();
+            this.btn_menu = new System.Windows.Forms.Button();
+            this.pnl_catalogo = new System.Windows.Forms.Panel();
+            this.btn_categoria = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_carrito = new System.Windows.Forms.Button();
+            this.pnl_consultas = new System.Windows.Forms.Panel();
+            this.btn_consultas = new System.Windows.Forms.Button();
+            this.pnl_salir = new System.Windows.Forms.Panel();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.navegador.SuspendLayout();
+            this.pnl_menu.SuspendLayout();
+            this.pnl_catalogo.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.pnl_consultas.SuspendLayout();
+            this.pnl_salir.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navegador
+            // 
+            this.navegador.Controls.Add(this.pnl_menu);
+            this.navegador.Controls.Add(this.pnl_catalogo);
+            this.navegador.Controls.Add(this.panel1);
+            this.navegador.Controls.Add(this.pnl_consultas);
+            this.navegador.Controls.Add(this.pnl_salir);
+            this.navegador.Dock = System.Windows.Forms.DockStyle.Left;
+            this.navegador.Location = new System.Drawing.Point(0, 0);
+            this.navegador.Name = "navegador";
+            this.navegador.Size = new System.Drawing.Size(289, 911);
+            this.navegador.TabIndex = 1;
+            // 
+            // pnl_menu
+            // 
+            this.pnl_menu.Controls.Add(this.btn_menu);
+            this.pnl_menu.Location = new System.Drawing.Point(3, 30);
+            this.pnl_menu.Margin = new System.Windows.Forms.Padding(3, 30, 3, 30);
+            this.pnl_menu.Name = "pnl_menu";
+            this.pnl_menu.Size = new System.Drawing.Size(286, 60);
+            this.pnl_menu.TabIndex = 1;
+            // 
+            // btn_menu
+            // 
+            this.btn_menu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menu.ForeColor = System.Drawing.Color.White;
+            this.btn_menu.Image = ((System.Drawing.Image)(resources.GetObject("btn_menu.Image")));
+            this.btn_menu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_menu.Location = new System.Drawing.Point(-5, -19);
+            this.btn_menu.Name = "btn_menu";
+            this.btn_menu.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_menu.Size = new System.Drawing.Size(331, 101);
+            this.btn_menu.TabIndex = 2;
+            this.btn_menu.Text = "        Menú";
+            this.btn_menu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_menu.UseVisualStyleBackColor = false;
+            // 
+            // pnl_catalogo
+            // 
+            this.pnl_catalogo.Controls.Add(this.btn_categoria);
+            this.pnl_catalogo.Location = new System.Drawing.Point(3, 123);
+            this.pnl_catalogo.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.pnl_catalogo.Name = "pnl_catalogo";
+            this.pnl_catalogo.Size = new System.Drawing.Size(286, 60);
+            this.pnl_catalogo.TabIndex = 1;
+            // 
+            // btn_categoria
+            // 
+            this.btn_categoria.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_categoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_categoria.ForeColor = System.Drawing.Color.White;
+            this.btn_categoria.Image = ((System.Drawing.Image)(resources.GetObject("btn_categoria.Image")));
+            this.btn_categoria.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_categoria.Location = new System.Drawing.Point(-12, -19);
+            this.btn_categoria.Name = "btn_categoria";
+            this.btn_categoria.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_categoria.Size = new System.Drawing.Size(331, 101);
+            this.btn_categoria.TabIndex = 3;
+            this.btn_categoria.Text = "        Catalogo";
+            this.btn_categoria.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_categoria.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_carrito);
+            this.panel1.Location = new System.Drawing.Point(3, 216);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_carrito
+            // 
+            this.btn_carrito.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_carrito.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_carrito.ForeColor = System.Drawing.Color.White;
+            this.btn_carrito.Image = ((System.Drawing.Image)(resources.GetObject("btn_carrito.Image")));
+            this.btn_carrito.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_carrito.Location = new System.Drawing.Point(-13, -20);
+            this.btn_carrito.Name = "btn_carrito";
+            this.btn_carrito.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_carrito.Size = new System.Drawing.Size(331, 101);
+            this.btn_carrito.TabIndex = 4;
+            this.btn_carrito.Text = "        Carrito ";
+            this.btn_carrito.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_carrito.UseVisualStyleBackColor = false;
+            // 
+            // pnl_consultas
+            // 
+            this.pnl_consultas.Controls.Add(this.btn_consultas);
+            this.pnl_consultas.Location = new System.Drawing.Point(3, 309);
+            this.pnl_consultas.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.pnl_consultas.Name = "pnl_consultas";
+            this.pnl_consultas.Size = new System.Drawing.Size(286, 60);
+            this.pnl_consultas.TabIndex = 1;
+            // 
+            // btn_consultas
+            // 
+            this.btn_consultas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_consultas.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_consultas.ForeColor = System.Drawing.Color.White;
+            this.btn_consultas.Image = ((System.Drawing.Image)(resources.GetObject("btn_consultas.Image")));
+            this.btn_consultas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_consultas.Location = new System.Drawing.Point(-5, -19);
+            this.btn_consultas.Name = "btn_consultas";
+            this.btn_consultas.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_consultas.Size = new System.Drawing.Size(331, 101);
+            this.btn_consultas.TabIndex = 5;
+            this.btn_consultas.Text = "        Consultas";
+            this.btn_consultas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_consultas.UseVisualStyleBackColor = false;
+            // 
+            // pnl_salir
+            // 
+            this.pnl_salir.Controls.Add(this.btn_salir);
+            this.pnl_salir.Location = new System.Drawing.Point(3, 829);
+            this.pnl_salir.Margin = new System.Windows.Forms.Padding(3, 430, 3, 3);
+            this.pnl_salir.Name = "pnl_salir";
+            this.pnl_salir.Size = new System.Drawing.Size(286, 60);
+            this.pnl_salir.TabIndex = 1;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.ForeColor = System.Drawing.Color.White;
+            this.btn_salir.Image = ((System.Drawing.Image)(resources.GetObject("btn_salir.Image")));
+            this.btn_salir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_salir.Location = new System.Drawing.Point(-7, -19);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_salir.Size = new System.Drawing.Size(331, 101);
+            this.btn_salir.TabIndex = 6;
+            this.btn_salir.Text = "        Salir";
+            this.btn_salir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_salir.UseVisualStyleBackColor = false;
+            // 
+            // frm_dashboard_cajero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1564, 911);
+            this.Controls.Add(this.navegador);
+            this.Name = "frm_dashboard_cajero";
+            this.Text = "frm_dashboard_cajero";
+            this.navegador.ResumeLayout(false);
+            this.pnl_menu.ResumeLayout(false);
+            this.pnl_catalogo.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.pnl_consultas.ResumeLayout(false);
+            this.pnl_salir.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.FlowLayoutPanel navegador;
+        protected System.Windows.Forms.Panel pnl_menu;
+        protected System.Windows.Forms.Button btn_menu;
+        protected System.Windows.Forms.Panel pnl_catalogo;
+        protected System.Windows.Forms.Button btn_categoria;
+        protected System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_carrito;
+        protected System.Windows.Forms.Panel pnl_consultas;
+        private System.Windows.Forms.Button btn_consultas;
+        private System.Windows.Forms.Panel pnl_salir;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
---
+++ poyecto_catedra_poo_supermecado/frm_dashboard_cajero.cs
@@ -0,0 +1,29 @@
+﻿using project_supermercado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace poyecto_catedra_poo_supermecado
+{
+    public partial class frm_dashboard_cajero : Form
+    {
+        public frm_dashboard_cajero()
+        {
+            InitializeComponent();
+            FormHelper.DefaultFormValues(this);
+            navegador.BackColor = Color.FromArgb(204, 0, 0);
+            btn_menu.BackColor = Color.FromArgb(204, 0, 0);
+            btn_carrito.BackColor = Color.FromArgb(204, 0, 0);
+            btn_categoria.BackColor = Color.FromArgb(204, 0, 0);
+            btn_menu.FlatAppearance.BorderColor = Color.FromArgb(204, 0, 0);
+            btn_salir.BackColor = Color.FromArgb(204, 0, 0);
+            btn_consultas.BackColor = Color.FromArgb(204, 0, 0);
+        }
+    }
+}
