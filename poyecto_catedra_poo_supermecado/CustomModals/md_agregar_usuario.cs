512e33841383cc64c87ca9f185244863abe9701f
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-28 14:15:01 -0600

    modal de agregado de usuairo

---
+++ poyecto_catedra_poo_supermecado/CustomModals/md_agregar_usuario.cs
@@ -0,0 +1,24 @@
+﻿using Catedra.CustomControls;
+using project_supermercado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace poyecto_catedra_poo_supermecado.CustomModals
+{
+    public partial class md_agregar_usuario : Form
+    {
+        public md_agregar_usuario()
+        {
+            InitializeComponent();
+            FormHelper.DefaultFormValues(this);
+            
+        }
+    }
+}
