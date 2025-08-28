5ca427c7b0c38bd6e3eb5eb7109821d3926dde79
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-28 00:17:10 -0600

    Agregar archivos de proyecto.

---
+++ poyecto_catedra_poo_supermecado/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace poyecto_catedra_poo_supermecado
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
