ffde518324304b7b48994be1f3ae7800fdeaac02
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-28 00:23:44 -0600

    login

---
+++ poyecto_catedra_poo_supermecado/frm_login.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace poyecto_catedra_poo_supermecado
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
+            this.panelRedondeado1 = new project_supermercado.CustomControls.PanelRedondeado();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonMaxing1 = new Catedra.CustomControls.ButtonMaxing();
+            this.textboxMaxing2 = new Catedra.CustomControls.TextboxMaxing();
+            this.textboxMaxing1 = new Catedra.CustomControls.TextboxMaxing();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelRedondeado1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelRedondeado1
+            // 
+            this.panelRedondeado1.BackColor = System.Drawing.Color.White;
+            this.panelRedondeado1.ColorBorde = System.Drawing.Color.White;
+            this.panelRedondeado1.ColorFin = System.Drawing.Color.White;
+            this.panelRedondeado1.ColorInicio = System.Drawing.Color.White;
+            this.panelRedondeado1.Controls.Add(this.label4);
+            this.panelRedondeado1.Controls.Add(this.label3);
+            this.panelRedondeado1.Controls.Add(this.label2);
+            this.panelRedondeado1.Controls.Add(this.label1);
+            this.panelRedondeado1.Controls.Add(this.buttonMaxing1);
+            this.panelRedondeado1.Controls.Add(this.textboxMaxing2);
+            this.panelRedondeado1.Controls.Add(this.textboxMaxing1);
+            this.panelRedondeado1.ForeColor = System.Drawing.Color.Black;
+            this.panelRedondeado1.Location = new System.Drawing.Point(306, 109);
+            this.panelRedondeado1.Name = "panelRedondeado1";
+            this.panelRedondeado1.Size = new System.Drawing.Size(398, 462);
+            this.panelRedondeado1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Clave";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingresa tus credenciales para poder entrar al sistema";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bienvenido";
+            // 
+            // buttonMaxing1
+            // 
+            this.buttonMaxing1.BackColor = System.Drawing.Color.Red;
+            this.buttonMaxing1.BackgroundColor = System.Drawing.Color.Red;
+            this.buttonMaxing1.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonMaxing1.BorderRadius = 15;
+            this.buttonMaxing1.BorderSize = 0;
+            this.buttonMaxing1.FlatAppearance.BorderSize = 0;
+            this.buttonMaxing1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMaxing1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMaxing1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonMaxing1.Location = new System.Drawing.Point(64, 327);
+            this.buttonMaxing1.Name = "buttonMaxing1";
+            this.buttonMaxing1.Size = new System.Drawing.Size(261, 63);
+            this.buttonMaxing1.TabIndex = 2;
+            this.buttonMaxing1.Text = "Ingresar";
+            this.buttonMaxing1.TextColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonMaxing1.UseVisualStyleBackColor = false;
+            // 
+            // textboxMaxing2
+            // 
+            this.textboxMaxing2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.textboxMaxing2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textboxMaxing2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textboxMaxing2.BorderSize = 7;
+            this.textboxMaxing2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxMaxing2.ForeColor = System.Drawing.Color.DimGray;
+            this.textboxMaxing2.Location = new System.Drawing.Point(64, 239);
+            this.textboxMaxing2.Margin = new System.Windows.Forms.Padding(4);
+            this.textboxMaxing2.Multiline = false;
+            this.textboxMaxing2.Name = "textboxMaxing2";
+            this.textboxMaxing2.Padding = new System.Windows.Forms.Padding(7);
+            this.textboxMaxing2.PasswordChar = false;
+            this.textboxMaxing2.Size = new System.Drawing.Size(261, 35);
+            this.textboxMaxing2.TabIndex = 1;
+            this.textboxMaxing2.Texts = "";
+            this.textboxMaxing2.UnderlinedStyle = true;
+            // 
+            // textboxMaxing1
+            // 
+            this.textboxMaxing1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.textboxMaxing1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textboxMaxing1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textboxMaxing1.BorderSize = 7;
+            this.textboxMaxing1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxMaxing1.ForeColor = System.Drawing.Color.DimGray;
+            this.textboxMaxing1.Location = new System.Drawing.Point(64, 154);
+            this.textboxMaxing1.Margin = new System.Windows.Forms.Padding(4);
+            this.textboxMaxing1.Multiline = false;
+            this.textboxMaxing1.Name = "textboxMaxing1";
+            this.textboxMaxing1.Padding = new System.Windows.Forms.Padding(7);
+            this.textboxMaxing1.PasswordChar = false;
+            this.textboxMaxing1.Size = new System.Drawing.Size(261, 35);
+            this.textboxMaxing1.TabIndex = 0;
+            this.textboxMaxing1.Texts = "";
+            this.textboxMaxing1.UnderlinedStyle = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1021, 678);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 681);
+            this.Controls.Add(this.panelRedondeado1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frm_login";
+            this.Text = "frm_login";
+            this.panelRedondeado1.ResumeLayout(false);
+            this.panelRedondeado1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private project_supermercado.CustomControls.PanelRedondeado panelRedondeado1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Catedra.CustomControls.ButtonMaxing buttonMaxing1;
+        private Catedra.CustomControls.TextboxMaxing textboxMaxing2;
+        private Catedra.CustomControls.TextboxMaxing textboxMaxing1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
