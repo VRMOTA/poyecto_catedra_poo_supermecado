32bd3e9c16cdc5d50401405edd8ce0dcd7f80110
StannDubon <stalin.dbn@gmail.com>
2025-08-28 01:24:39 -0600

    cambios generales

--- poyecto_catedra_poo_supermecado/CustomCards/card_producto_carrito.Designer.cs
+++ poyecto_catedra_poo_supermecado/CustomCards/card_producto_carrito.Designer.cs
@@ -34,25 +34,30 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonMaxing2 = new Catedra.CustomControls.ButtonMaxing();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonMaxing1
             // 
-            this.buttonMaxing1.BackColor = System.Drawing.Color.Red;
-            this.buttonMaxing1.BackgroundColor = System.Drawing.Color.Red;
+            this.buttonMaxing1.BackColor = System.Drawing.Color.DimGray;
+            this.buttonMaxing1.BackgroundColor = System.Drawing.Color.DimGray;
             this.buttonMaxing1.BorderColor = System.Drawing.Color.WhiteSmoke;
-            this.buttonMaxing1.BorderRadius = 15;
+            this.buttonMaxing1.BorderRadius = 10;
             this.buttonMaxing1.BorderSize = 0;
             this.buttonMaxing1.FlatAppearance.BorderSize = 0;
             this.buttonMaxing1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonMaxing1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonMaxing1.ForeColor = System.Drawing.Color.White;
-            this.buttonMaxing1.Location = new System.Drawing.Point(412, 118);
+            this.buttonMaxing1.Location = new System.Drawing.Point(620, 144);
             this.buttonMaxing1.Name = "buttonMaxing1";
-            this.buttonMaxing1.Size = new System.Drawing.Size(188, 30);
+            this.buttonMaxing1.Size = new System.Drawing.Size(123, 30);
             this.buttonMaxing1.TabIndex = 11;
-            this.buttonMaxing1.Text = "Visualizar";
+            this.buttonMaxing1.Text = "Eliminar";
             this.buttonMaxing1.TextColor = System.Drawing.Color.White;
             this.buttonMaxing1.UseVisualStyleBackColor = false;
             // 
@@ -60,17 +65,18 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(660, 41);
+            this.label3.Location = new System.Drawing.Point(683, 29);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "$0.00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(409, 41);
+            this.label4.Location = new System.Drawing.Point(260, 29);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 16);
             this.label4.TabIndex = 9;
@@ -81,18 +87,19 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(660, 76);
+            this.label2.Location = new System.Drawing.Point(683, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 16);
             this.label2.TabIndex = 8;
             this.label2.Text = "$0.00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(409, 76);
+            this.label1.Location = new System.Drawing.Point(260, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 16);
             this.label1.TabIndex = 7;
@@ -101,17 +108,83 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::poyecto_catedra_poo_supermecado.Properties.Resources._default;
-            this.pictureBox1.Location = new System.Drawing.Point(51, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(17, 20);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(188, 119);
+            this.pictureBox1.Size = new System.Drawing.Size(218, 154);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(260, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cantidad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(707, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(683, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "$0.00";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(260, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Total";
+            // 
+            // buttonMaxing2
+            // 
+            this.buttonMaxing2.BackColor = System.Drawing.Color.Red;
+            this.buttonMaxing2.BackgroundColor = System.Drawing.Color.Red;
+            this.buttonMaxing2.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonMaxing2.BorderRadius = 10;
+            this.buttonMaxing2.BorderSize = 0;
+            this.buttonMaxing2.FlatAppearance.BorderSize = 0;
+            this.buttonMaxing2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMaxing2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMaxing2.ForeColor = System.Drawing.Color.White;
+            this.buttonMaxing2.Location = new System.Drawing.Point(491, 144);
+            this.buttonMaxing2.Name = "buttonMaxing2";
+            this.buttonMaxing2.Size = new System.Drawing.Size(123, 30);
+            this.buttonMaxing2.TabIndex = 16;
+            this.buttonMaxing2.Text = "Actualizar";
+            this.buttonMaxing2.TextColor = System.Drawing.Color.White;
+            this.buttonMaxing2.UseVisualStyleBackColor = false;
             // 
             // card_producto_carrito
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonMaxing2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.buttonMaxing1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
@@ -119,7 +192,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "card_producto_carrito";
-            this.Size = new System.Drawing.Size(793, 180);
+            this.Size = new System.Drawing.Size(775, 204);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -134,5 +207,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private Catedra.CustomControls.ButtonMaxing buttonMaxing2;
     }
 }
