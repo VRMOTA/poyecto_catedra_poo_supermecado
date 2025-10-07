fc5878a4ecd886ee1e07873153429b761c4a683e
VRMOTA <w0lf3ryxd@gmail.com>
2025-10-07 17:44:29 -0600

    as

---
+++ Datos(prueba conexion)/conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datosprueba
+{
+    public class conexion
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["sql"].ConnectionString);
+
+        public bool ProbarConexion()
+        {
+            try
+            {
+                cn.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+        }
+    }
+}
