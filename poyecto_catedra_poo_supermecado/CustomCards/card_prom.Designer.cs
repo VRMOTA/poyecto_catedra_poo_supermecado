c69ac9c87cdb588f54f8c565963d5ea200ab250b
VRMOTA <w0lf3ryxd@gmail.com>
2025-10-13 16:16:53 -0600

    Avances en promociones

--- poyecto_catedra_poo_supermecado/CustomCards/card_prom.Designer.cs
+++ poyecto_catedra_poo_supermecado/CustomCards/card_prom.Designer.cs
@@ -28,64 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.roundedControlBase1 = new poyecto_catedra_poo_supermecado.CustomCards.RoundedControlBase();
-            this.lblDescripcion = new System.Windows.Forms.Label();
-            this.lblDistribuidor = new System.Windows.Forms.Label();
             this.lblProducto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_cantidad_min = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_precio_prom = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_fecha_inicio = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lb_fecha_fin = new System.Windows.Forms.Label();
+            this.btnActualizar = new poyecto_catedra_poo_supermecado.CustomControls.ButtonMaxing();
+            this.btnEliminar = new poyecto_catedra_poo_supermecado.CustomControls.ButtonMaxing();
+            this.roundedControlBase1 = new poyecto_catedra_poo_supermecado.CustomCards.RoundedControlBase();
             this.SuspendLayout();
-            // 
-            // roundedControlBase1
-            // 
-            this.roundedControlBase1.BackColor = System.Drawing.Color.Transparent;
-            this.roundedControlBase1.BorderColor = System.Drawing.Color.Gray;
-            this.roundedControlBase1.BorderThickness = 1;
-            this.roundedControlBase1.CornerRadius = 12;
-            this.roundedControlBase1.FillColor = System.Drawing.Color.White;
-            this.roundedControlBase1.Location = new System.Drawing.Point(0, 0);
-            this.roundedControlBase1.Name = "roundedControlBase1";
-            this.roundedControlBase1.Size = new System.Drawing.Size(507, 150);
-            this.roundedControlBase1.TabIndex = 0;
-            // 
-            // lblDescripcion
-            // 
-            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescripcion.Location = new System.Drawing.Point(18, 78);
-            this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(470, 56);
-            this.lblDescripcion.TabIndex = 36;
-            this.lblDescripcion.Text = "Descripcion";
-            // 
-            // lblDistribuidor
-            // 
-            this.lblDistribuidor.AutoSize = true;
-            this.lblDistribuidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDistribuidor.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.lblDistribuidor.Location = new System.Drawing.Point(18, 43);
-            this.lblDistribuidor.Name = "lblDistribuidor";
-            this.lblDistribuidor.Size = new System.Drawing.Size(95, 18);
-            this.lblDistribuidor.TabIndex = 35;
-            this.lblDistribuidor.Text = "Distribuidor";
             // 
             // lblProducto
             // 
             this.lblProducto.AutoSize = true;
+            this.lblProducto.BackColor = System.Drawing.Color.White;
             this.lblProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProducto.Location = new System.Drawing.Point(18, 16);
+            this.lblProducto.Location = new System.Drawing.Point(15, 12);
+            this.lblProducto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblProducto.Name = "lblProducto";
             this.lblProducto.Size = new System.Drawing.Size(142, 18);
             this.lblProducto.TabIndex = 34;
             this.lblProducto.Text = "Nombre Producto";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(15, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 18);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Cantidad Minima:";
+            // 
+            // lb_cantidad_min
+            // 
+            this.lb_cantidad_min.AutoSize = true;
+            this.lb_cantidad_min.BackColor = System.Drawing.Color.White;
+            this.lb_cantidad_min.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_cantidad_min.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_cantidad_min.Location = new System.Drawing.Point(167, 49);
+            this.lb_cantidad_min.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_cantidad_min.Name = "lb_cantidad_min";
+            this.lb_cantidad_min.Size = new System.Drawing.Size(16, 18);
+            this.lb_cantidad_min.TabIndex = 36;
+            this.lb_cantidad_min.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(15, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 18);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Precio Promocional:";
+            // 
+            // lb_precio_prom
+            // 
+            this.lb_precio_prom.AutoSize = true;
+            this.lb_precio_prom.BackColor = System.Drawing.Color.White;
+            this.lb_precio_prom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_precio_prom.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_precio_prom.Location = new System.Drawing.Point(167, 83);
+            this.lb_precio_prom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_precio_prom.Name = "lb_precio_prom";
+            this.lb_precio_prom.Size = new System.Drawing.Size(16, 18);
+            this.lb_precio_prom.TabIndex = 38;
+            this.lb_precio_prom.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(15, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Descripcion";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.BackColor = System.Drawing.Color.White;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(15, 155);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(611, 69);
+            this.lblDescripcion.TabIndex = 40;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(273, 49);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 18);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Fecha Inicio:";
+            // 
+            // lb_fecha_inicio
+            // 
+            this.lb_fecha_inicio.AutoSize = true;
+            this.lb_fecha_inicio.BackColor = System.Drawing.Color.White;
+            this.lb_fecha_inicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fecha_inicio.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_fecha_inicio.Location = new System.Drawing.Point(372, 49);
+            this.lb_fecha_inicio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_fecha_inicio.Name = "lb_fecha_inicio";
+            this.lb_fecha_inicio.Size = new System.Drawing.Size(16, 18);
+            this.lb_fecha_inicio.TabIndex = 42;
+            this.lb_fecha_inicio.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(287, 83);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 18);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Fecha Fin:";
+            // 
+            // lb_fecha_fin
+            // 
+            this.lb_fecha_fin.AutoSize = true;
+            this.lb_fecha_fin.BackColor = System.Drawing.Color.White;
+            this.lb_fecha_fin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fecha_fin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_fecha_fin.Location = new System.Drawing.Point(372, 83);
+            this.lb_fecha_fin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_fecha_fin.Name = "lb_fecha_fin";
+            this.lb_fecha_fin.Size = new System.Drawing.Size(16, 18);
+            this.lb_fecha_fin.TabIndex = 44;
+            this.lb_fecha_fin.Text = "0";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.Red;
+            this.btnActualizar.BackgroundColor = System.Drawing.Color.Red;
+            this.btnActualizar.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnActualizar.BorderRadius = 10;
+            this.btnActualizar.BorderSize = 0;
+            this.btnActualizar.FlatAppearance.BorderSize = 0;
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.ForeColor = System.Drawing.Color.White;
+            this.btnActualizar.Location = new System.Drawing.Point(290, 245);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(164, 37);
+            this.btnActualizar.TabIndex = 46;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.TextColor = System.Drawing.Color.White;
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.DimGray;
+            this.btnEliminar.BackgroundColor = System.Drawing.Color.DimGray;
+            this.btnEliminar.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnEliminar.BorderRadius = 10;
+            this.btnEliminar.BorderSize = 0;
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(462, 245);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(164, 37);
+            this.btnEliminar.TabIndex = 45;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.TextColor = System.Drawing.Color.White;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // roundedControlBase1
+            // 
+            this.roundedControlBase1.BackColor = System.Drawing.SystemColors.Control;
+            this.roundedControlBase1.BorderColor = System.Drawing.SystemColors.Control;
+            this.roundedControlBase1.BorderThickness = 1;
+            this.roundedControlBase1.CornerRadius = 12;
+            this.roundedControlBase1.FillColor = System.Drawing.Color.White;
+            this.roundedControlBase1.Location = new System.Drawing.Point(0, 0);
+            this.roundedControlBase1.Margin = new System.Windows.Forms.Padding(4);
+            this.roundedControlBase1.Name = "roundedControlBase1";
+            this.roundedControlBase1.Size = new System.Drawing.Size(639, 300);
+            this.roundedControlBase1.TabIndex = 0;
+            // 
             // card_prom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lb_fecha_fin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lb_fecha_inicio);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.lblDescripcion);
-            this.Controls.Add(this.lblDistribuidor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb_precio_prom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_cantidad_min);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblProducto);
             this.Controls.Add(this.roundedControlBase1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "card_prom";
-            this.Size = new System.Drawing.Size(507, 150);
+            this.Size = new System.Drawing.Size(638, 300);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,8 +266,18 @@
         #endregion
 
         private RoundedControlBase roundedControlBase1;
-        private System.Windows.Forms.Label lblDescripcion;
-        private System.Windows.Forms.Label lblDistribuidor;
         private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_cantidad_min;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_precio_prom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_fecha_inicio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lb_fecha_fin;
+        private CustomControls.ButtonMaxing btnActualizar;
+        private CustomControls.ButtonMaxing btnEliminar;
     }
 }
